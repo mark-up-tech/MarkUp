51f6d2d66556c2d1fb36766af3216ad5f7c7b423
Mark Up <markup.scrum@gmail.com>
2020-11-04 21:10:48 +0800

    added roles column

--- BackEndFiles/accounts.sql
+++ BackEndFiles/accounts.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 04, 2020 at 01:19 PM
+-- Generation Time: Nov 04, 2020 at 02:09 PM
 -- Server version: 10.4.14-MariaDB
 -- PHP Version: 7.4.11
 
@@ -35,16 +35,19 @@ CREATE TABLE `accounts` (
   `Email` varchar(128) NOT NULL,
   `UserPwd` varchar(128) NOT NULL,
   `Date_Created` timestamp NOT NULL DEFAULT current_timestamp(),
-  `Status` int(1) NOT NULL DEFAULT 1
+  `Status` int(1) NOT NULL DEFAULT 1,
+  `Role` int(1) NOT NULL DEFAULT 1
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `accounts`
 --
 
-INSERT INTO `accounts` (`ID`, `First_Name`, `Last_Name`, `Contact_Num`, `Email`, `UserPwd`, `Date_Created`, `Status`) VALUES
-(22, 'jonathan', 'Lozano', 12333, 'kyle@gmail.com', '$2y$10$hGqyKiBqu/v.lCGlZl7pUOvyyESPQYCd4sp/xw8OEGJmuM3rsZcuS', '2020-11-02 15:03:25', 1),
-(23, 'wap', 'los', 9094565, 'kyle2@gmail.com', '$2y$10$txSfqUy2ER.3lpAN0H/1cOOIn/ryxd6L6HJ29.9L4jhcnb4Agywma', '2020-11-02 15:12:46', 1);
+INSERT INTO `accounts` (`ID`, `First_Name`, `Last_Name`, `Contact_Num`, `Email`, `UserPwd`, `Date_Created`, `Status`, `Role`) VALUES
+(22, 'jonathan', 'Lozano', 12333, 'kyle@gmail.com', '$2y$10$hGqyKiBqu/v.lCGlZl7pUOvyyESPQYCd4sp/xw8OEGJmuM3rsZcuS', '2020-11-02 15:03:25', 1, 1),
+(23, 'wap', 'los', 9094565, 'kyle2@gmail.com', '$2y$10$txSfqUy2ER.3lpAN0H/1cOOIn/ryxd6L6HJ29.9L4jhcnb4Agywma', '2020-11-02 15:12:46', 1, 1),
+(24, 'test1fn', 'test1ln', 9321654987, 'test1@gmail.com', '$2y$10$9WAqRnUa/NqbDVdG6k7BU.wJKG66h8HuExjNadK6xSVhfGNZA5JBa', '2020-11-04 12:28:38', 1, 1),
+(25, 'test2fn', 'test2ln', 94654632, 'test2ln@gmail.com', '$2y$10$NOwM78xOgg2VvMZawgGlOu.t8QQwcD/NbFAOa3sSKCBft6Jbfvc3O', '2020-11-04 12:47:29', 1, 1);
 
 --
 -- Indexes for dumped tables
@@ -67,7 +70,7 @@ ALTER TABLE `accounts`
 -- AUTO_INCREMENT for table `accounts`
 --
 ALTER TABLE `accounts`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
